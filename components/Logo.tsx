14e68036963b4a30962e25e29a1edbfd4d26e417
SHIVA1633 <kurhade.shivshankar@gmail.com>
2025-01-22 05:24:17 +0530

    Update Logo.tsx

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,7 +4,7 @@ export function Logo() {
   return (
     <div className="h-12 sm:h-20 relative">
       <Image
-        src="https://hebbkx1anhila5yf.public.blob.vercel-storage.com/image%20(1)-NQQgrYiSh4Xy6jfKs1Q49hqJ4DyhLF.png"
+        src="/placeholder-logo.png"
         alt="Futuredge Solutions Logo"
         width={350}
         height={100}
